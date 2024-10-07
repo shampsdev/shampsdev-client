bdab1df17989a3d3844f2f0ceff81d603d290650
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-07 20:05:05 +0300

    added docker support

--- src/modules/team.section.tsx
+++ src/modules/team.section.tsx
@@ -1,3 +1,17 @@
+
+
+
 export const TeamSection = () => {
-  return <></>
-}
+  return (
+    <div className='min-h-screen'>
+      <p className='text-center text-xl py-5'>Команда</p>
+      <div className='grid md:grid-cols-3 w-[90%] mx-auto'>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+      </div>
+      <p className='text-center text-xl pt-5'>Отдельное спасибо тем, кто поддержал нас!</p>
+      <p className='text-center text-muted-foreground text-xl pb-5'>Целуем в носик за ваши пару строчек</p>
+    </div>
+  );
+};
