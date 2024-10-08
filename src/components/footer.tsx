02122c8787ae04b66191ebfd17fa9ef9c2398eb4
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-08 14:56:58 +0300

    updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     <>
       <img className='md:max-w-[50%] mx-auto' src='/people/team.gif' alt='' />
       <div className='flex flex-col justify-between bg-black text-white w-full rounded-t-3xl min-h-[30vh] px-16 pt-14 pb-10'>
-        <div className='flex justify-between h-full flex-col md:flex-row'>
+        <div className='flex justify-between h-full flex-col md:flex-row md:gap-0 gap-10 pb-10'>
           <div>
             <TextLogo.full className='w-48 h-fit pb-2' />
             <p className='text-white/50 w-72 text-sm'>
@@ -14,7 +14,7 @@ export const Footer = () => {
               из ИТМО!
             </p>
           </div>
-          <div className='flex text-xl gap-10 h-fit flex-col md:flex-row'>
+          <div className='flex text-xl md:gap-10 h-fit flex-col md:flex-row'>
             <div>
               <p>Главная</p>
               <p>Проекты</p>
@@ -24,10 +24,12 @@ export const Footer = () => {
               <p>Команда</p>
               <p>Блог</p>
             </div>
-            <div className='flex gap-5'>
-              <Icons.instagram className='cursor-pointer' />
-              <Icons.telegram className='cursor-pointer' />
-              <Icons.youtube className='cursor-pointer' />
+            <div className='flex items-center md:items-start pt-10 md:pt-0'>
+              <div className='flex h-fit items-center gap-5'>
+                <Icons.instagram className='cursor-pointer' />
+                <Icons.telegram className='cursor-pointer' />
+                <Icons.youtube className='cursor-pointer' />
+              </div>
             </div>
           </div>
         </div>
