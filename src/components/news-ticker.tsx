cccc9f7b0ca40b4ce5707929dfd7031369a71a6b
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-10 14:19:42 +0300

    made changes to the news ticker

--- src/components/news-ticker.tsx
+++ src/components/news-ticker.tsx
@@ -2,23 +2,34 @@ import { News } from '@/types/news.interface';
 import Marquee from 'react-fast-marquee';
 
 interface NewsTickerProps {
-  news: News[];
+  newsTop: News[];
+  newsBottom: News[];
 }
 
-export const NewsTicker = ({ news }: NewsTickerProps) => {
-  console.log(news);
-
+export const NewsTicker = ({ newsTop, newsBottom }: NewsTickerProps) => {
   return (
     <div className='py-10'>
-      <hr className='w-[90%] mx-auto ' />
-      <Marquee className='flex justify-around py-4 gap-2'>
-        {news.map((x) => (
-          <div className='pr-10'>
+      {/* <hr className='w-[90%] mx-auto ' /> */}
+      <Marquee
+        direction='right'
+        autoFill
+        className='flex justify-around py-4 gap-2'
+      >
+        {newsTop.map((x) => (
+          <div className='pr-10 flex items-center gap-5'>
             {x.icon} <span className='text-xl'>{x.title}</span>
           </div>
         ))}
       </Marquee>
-      <hr className='w-[90%] mx-auto ' />
+      {/* <hr className='w-[90%] mx-auto ' /> */}
+      <Marquee autoFill className='flex justify-around py-4 gap-2'>
+        {newsBottom.map((x) => (
+          <div className='pr-10 flex items-center gap-5'>
+            {x.icon} <span className='text-xl'>{x.title}</span>
+          </div>
+        ))}
+      </Marquee>
+      {/* <hr className='w-[90%] mx-auto ' /> */}
     </div>
   );
 };
