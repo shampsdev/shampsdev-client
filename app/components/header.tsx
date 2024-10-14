ee8be56511435a90f1f38bb8ac4485b2d00280b0
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-14 17:45:48 +0300

    naive migration to nextjs

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Icons } from '@/components/icons';
 import { useState } from 'react';
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './index.css'
+import './globals.css'
 
 export default function RootLayout({
   children,
